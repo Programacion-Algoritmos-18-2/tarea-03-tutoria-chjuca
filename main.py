00f239d2382baa3dae07cef921fd83e32b4ba8bf
Carlos Juca <chjuca@utpl.edu.ec>
2018-10-31 22:12:07 -0500

    Actualizando Actividad tutoria 003

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from paquete1.modelado import *
+from paquete1.modelado import *			# Importamos todos los elementos que se encuentan en el paquete modelado
 
-d=Docente("Docente BD","Loja")
-d2=Docente("Docente Prog","Quito")
-listado=[d,d2]
-e=Estudiante("Luis",listado)
-print(e.presentar_datos())
+d=Docente("Docente BD","Loja")			# Creamos un objeto Docente y le enviamos los 2 parametros
+d2=Docente("Docente Prog","Quito")		# Creamos otro objeto Docente y le enviamos los 2 parametros
+listado=[d,d2]							# Almacenamos ambos objetos en una lista
+e=Estudiante("Luis",listado)			# Creamos un objeto Estudiante y enviamos una cadena y la lista 
+print(e.presentar_datos())				# Presentamos la cadena que nos devuelva la funcion presentar_datos del objeto Estudiante
--- paquete1/modelado.py
+++ paquete1/modelado.py
@@ -1,39 +1,48 @@
-class Docente:
- 	"""docstring for Docente"""
- 	def __init__(self,n,a):
+class Docente:									# Creamos la Clase Docente
+
+ 	def __init__(self,n,a):						# Creamos el Metodo __init__
+ 		self.nombre=n 							# Atributo nombre de la clase Docente 
+ 		self.ciudad=a 							# Atributo ciudad de la clase Docente 
+
+ 	def setNombre(self,n):						#Funcion da valor n el atributo nombre
  		self.nombre=n
+
+ 	def setCiudad(self,a):						#Funcion da valor a el atributo ciudad
  		self.ciudad=a
- 	def setNombre(self,n):
- 		self.nombre=n
- 	def setCiudad(self,a):
- 		self.ciudad=a
- 	def getNombre(self):
+
+ 	def getNombre(self):						#Esta funcion devuelve el atributo nombre
  		return self.nombre
- 	def getCiudad(self):
+
+ 	def getCiudad(self):						#Esta funcion devuelve el atributo ciudad
  		return self.ciudad
  		
- 	def presentar_datos(self):
+ 	def presentar_datos(self):					# Presentamos los valores de los atributos nombre y ciudad
  		cadena="%s\n\t%s"%(self.getNombre(),self.getCiudad())
 	
-class Estudiante:
+
+class Estudiante:								# Creamos la Clase Estudiante
 	"""docstring for Estudiante"""
-	def __init__(self,n,listaDocentes):
+	def __init__(self,n,listaDocentes):			# Creamos el Metodo __init__
+		self.nombres=n 							# Atributo nombres de la clase Estudiante 
+		self.docentes=listaDocentes				# Atributo listaDocente la clase Estudiante 
+
+	def setNombre(self,n):						#Funcion da valor n el atributo nombre
 		self.nombres=n
+
+	def setDocentes(self,listaDocentes):		#Funcion da valor listaDocentes el atributo docentes
 		self.docentes=listaDocentes
-	def setNombre(self,n):
-		self.nombres=n
-	def setDocentes(self,listaDocentes):
-		self.docentes=listaDocentes
-	def getNombre(self):
+
+	def getNombre(self):						#Esta funcion devuelve el atributo nombre
 		return self.nombres
-	def getDocentes(self):
+
+	def getDocentes(self):						#Esta funcion devuelve el atributo docentes
 		return self.docentes
 
-	def presentar_datos(self):
-		cadena="\nEstudiante: %s\n"%(self.getNombre())
+	def presentar_datos(self):					#Presentamos los valores de los atributos Nombre 
+		cadena="\nEstudiante: %s\n"%(self.getNombre())	
 		cadena="%s %s"%(cadena,"Lista Docentes: ")
-		for i in range(0,len(self.docentes)):
-			cadena="%s\n\t-%s\n\t\t%s\n"%(cadena,self.docentes[i].getNombre(),self.docentes[i].getCiudad())
+		for i in self.getDocentes():
+			cadena="%s\n\t-%s\n\t\t%s\n"%(cadena,i.getNombre(),i.getCiudad())
 		return cadena
 
 
